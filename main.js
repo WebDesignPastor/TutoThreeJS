b7d06c42cef5ec90a75e72db3a5bdf27bd565663
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-05-15 09:07:43 +0200

    Resized to screen height width

--- main.js
+++ main.js
@@ -11,11 +11,29 @@ const material = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh)
 
+// Sizes
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+// Light
+
+const light = new THREE.PointLight(0xffffff, 1, 100)
+light.position.set(0, 10, 10)
+scene.add(light)
+
 // Camera
 
-const camera = new THREE.PerspectiveCamera(45, 800, 600)
+const camera = new THREE.PerspectiveCamera(45,
+  sizes.width / sizes.height,
+  0.1, 100)
+camera.position.z = 20
 scene.add(camera)
 
 // Render
 const canvas = document.querySelector('.webgl')
-const render = new THREE.WebGLRenderer({ canvas })
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
