15c67de37760509246b30386d5cdee5ffdeca63a
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-05-16 09:01:52 +0200

    Finished project

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const scene = new THREE.Scene();
 const geometry = new THREE.SphereGeometry(3, 64, 64)
 const material = new THREE.MeshStandardMaterial({
   color: '#ffd700',
+  roughness: 0.5
 })
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh)
@@ -26,6 +27,7 @@ const sizes = {
 
 const light = new THREE.PointLight(0xffffff, 1, 100)
 light.position.set(0, 10, 10)
+light.intensity = 1.25
 scene.add(light)
 
 // Camera
@@ -77,3 +79,24 @@ const tl = gsap.timeline({defaults: {duration: 1}})
 tl.fromTo(mesh.scale, {z: 0, x: 0, y: 0}, { z:1, x:1, y:1 })
 tl.fromTo('nav', { y: "-100%"}, { y: "0%"})
 tl.fromTo(".title", { opacity: 0 }, {opacity: 1})
+
+// Mouse animation color
+
+let mouseDown = false
+let rgb = [12, 23, 55];
+window.addEventListener('mousedown', () => (mouseDown = true ))
+window.addEventListener('mouseup', () => (mouseDown = false ))
+
+window.addEventListener('mousemove', (e) => {
+  if(mouseDown){
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ]
+
+    // Animate
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
+    gsap.to(mesh.material.color, {r: newColor.r, g: newColor.g, b: newColor.b})
+  }
+});
